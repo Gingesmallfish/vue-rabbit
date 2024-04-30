158eccd9a2b9787ecdb8bac7b62c8c2c4acbb986
姜小鱼 <1847535232@qq.com>
2024-04-30 16:16:49 +0800

    fest: "登录-401, token有bug"

--- src/stores/user.js
+++ src/stores/user.js
@@ -11,10 +11,16 @@ export const useUserStore = defineStore('user', () => {
         const res = await loginAPI({ account, password });
         userInfo.value = res.result
     }
+
+    // 退出清楚用户信息
+    const clearUserInfo = () => {
+        userInfo.value = {}
+    }
     // 3. 以对象的格式state和action return
     return {
         userInfo,
-        getUserInfo
+        getUserInfo,
+        clearUserInfo
     }
 }, {
     persist: true,
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import 'element-plus/es/components/message/style/css'
+import { useUserStore } from '@/stores/user'
 import { ElMessage } from 'element-plus'
+import {useRouter} from "vue-router";
 // 创建axios实例
 const http = axios.create({
     baseURL: 'https://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -9,16 +11,31 @@ const http = axios.create({
 
 // axios请求拦截器
 http.interceptors.request.use(config => {
+    // ? 1.从pinia获取token数据
+    const userStore = useUserStore()
+    // ? 2. 按照后端的要求拼接token数据
+    const token  = userStore.userInfo.token;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
 }, e => Promise.reject(e))
 
 // axios响应式拦截器
 http.interceptors.response.use(res => res.data, e => {
+    const userStore = useUserStore()
     // 统一错误提示
     ElMessage({ 
         type: 'warning',
         message: e.response.data.message
      })
+     // !401token 失效处理
+     // 1. 清除本地用户数据
+     // 2. 跳转到登录页
+    if (e.response.status === 401) {
+        userStore.clearUserInfo();
+      
+    }
     return Promise.reject(e)
 })
 
